686c64140dbf5f2256d063e6afece21a1ba0ec62
studymachine <1771134505@qq.com>
2022-08-05 16:09:18 +0800

    新增school模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AppService } from './app.service';
 import ormConfig from './config/orm.config';
 import ormConfigProd from './config/orm.config.prod';
 import { EventsModule } from './events/events.module';
+import { SchoolModule } from './school/school.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { EventsModule } from './events/events.module';
         process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
     }),
     EventsModule,
+    SchoolModule,
   ],
   controllers: [AppController],
   providers: [
--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -2,6 +2,8 @@ import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Attendee } from 'src/events/attendee.entity';
 import { EventEntity } from 'src/events/event.entity';
+import { Subject } from 'src/school/subject.entity';
+import { Teacher } from 'src/school/teacher.entity';
 
 export default registerAs(
   'orm.config',
@@ -12,7 +14,7 @@ export default registerAs(
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: [EventEntity, Attendee],
+    entities: [EventEntity, Attendee, Subject, Teacher],
     synchronize: true,
   }),
 );
